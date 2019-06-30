7c8f731e18db3c35c04592d2a001e05440048baf
jalasantosh <jala.santoshreddy@gmail.com>
2019-06-29 23:19:20 -0500

    react basics

---
+++ payroll/build/precache-manifest.d2c5c0f25f85ba14635eb5ef58d617c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d6630b19067af9d60633083ea9c1df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18b517dd9e85ea85c2b7",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e56b8ca974470e57c05f",
+    "url": "/static/js/2.876c3f7c.chunk.js"
+  },
+  {
+    "revision": "18b517dd9e85ea85c2b7",
+    "url": "/static/js/main.3482f74b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
